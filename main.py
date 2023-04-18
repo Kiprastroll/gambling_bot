7f88bffc137d78531ade9a78140e0d5606715c47
Kiprastrol <129788881+Kiprastrol@users.noreply.github.com>
2023-04-18 11:21:21 +0300

    Update main.py

--- main.py
+++ main.py
@@ -110,10 +110,10 @@ while True:
 		# Probability calculations
 
 		if last2col[0] != "g":
- 	 probabilitySameCol = (chances[0] / 15) ** lastam
- 	 print(str(probabilitySameCol))
-  probabilityGreen = 1 - ((15 - chances[2]) / 15)
-  probabilityOtherCol = 1 - probabilityGreen - probabilitySameCol
+ 	 		probabilitySameCol = (chances[0] / 15) ** lastam
+ 	 		print(str(probabilitySameCol))
+  		probabilityGreen = 1 - ((15 - chances[2]) / 15)
+  		probabilityOtherCol = 1 - probabilityGreen - probabilitySameCol
 
 		time.sleep(1)
 		doCalc = False
