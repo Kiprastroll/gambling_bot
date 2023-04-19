169ce4ff3e2d97bbe643ab52dc0af76f75268516
Kiprastroll <129764692+Kiprastroll@users.noreply.github.com>
2023-04-19 21:29:36 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
+import random
+
 import pyautogui
 import time
 import imagehash
 from PIL import Image
-
-# import threading
+import threading
 
 # x, y, width, height, difference
 
@@ -19,6 +20,11 @@ x = y = width = height = diff = 0
 doCheck = True
 did10 = doComm = doCalc = False
 answer = None
+targetRed = {}
+targetBlack = {}
+targetGreen = {}
+seed = "0010012345"  # num(3), bet(1), values(18 * n)
+ifBet = False
 
 
 def reqs(arg):
@@ -73,8 +79,45 @@ def shorten():
 	lastShort.append(str(bulk) + str(lastRolls[-1]))
 
 
-out100 = reqs("show last 100 basic? ")
-comb100 = reqs("show combined? ")  # TODO
+def start_threads(n):
+	for thr in range(1, n+1):
+		thread_name = "simulationThread" + str(thr)
+		t = threading.Thread(name=thread_name, target=thread_function, args=(thr,))
+		t.start()
+
+
+def thread_function(num):
+	global targetRed, targetGreen, targetBlack
+	print("Thread Number " + str(num) + " ----------------- \n")
+	if last2col[0] == "g":
+		print("last roll was green")
+	elif last2col[0] == "r":
+		print("last roll was red")
+	elif last2col[0] == "b":
+		print("last roll was black")
+	print(targetRed)
+
+
+
+out100 = reqs("show last 100 basic? (y/n) ")
+comb100 = reqs("show combined? (y/n) ")  # TODO
+loadSeed = reqs("load a seed? (y/n) ")
+if not loadSeed:
+	numSimulations = int(input("Number of threads: (int) "))
+	for o in range(1, numSimulations + 1):
+		targetRed[o] = round(random.uniform(0, 1), 4)
+		targetBlack[o] = round(random.uniform(0, 1), 4)
+		targetGreen[o] = round(random.uniform(0, 1), 4)
+else:
+	numSimulations = int(seed[:3])
+	for o in range(1, numSimulations + 1):
+		targetRed[o] = int(seed[(4 + (1 - 1) * 18):][:(6 + (1 - 1) * 18)]) / 100000
+		# targetBlack[o] = seed[(4 * o):][:(10 * o)]
+		# targetGreen[o] = seed[(4 * o):][:(10 * o)]
+cyclesGeneration = int(input("Cycles per generation: (int) "))
+
+
+
 
 while True:
 
@@ -111,7 +154,17 @@ while True:
 
 		if last2col[0] != "g":
 			probabilitySameCol = (chances[0] / 15) ** lastam
-			print(str(probabilitySameCol))
+		else:
+			probabilitySameCol = chances[0] / 15
+		probabilityGreen = 1 - ((15 - chances[2]) / 15)
+		probabilityOtherCol = 1 - probabilityGreen - probabilitySameCol
+		print("probability Green: " + str(probabilityGreen))
+		print("probability Other color: " + str(probabilityOtherCol))
+
+		# generation instances
+
+		start_threads(numSimulations)
+
 
 		time.sleep(1)
 		doCalc = False
